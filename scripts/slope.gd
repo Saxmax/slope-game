4b0ef4c966bf4a8d93a7c7934b20cb8cb9a096f3
Max Huber <max@maxhuber.se>
2025-07-17 01:09:07 +0200

    fixing

--- scripts/slope.gd
+++ scripts/slope.gd
@@ -1,7 +1,7 @@
 class_name Slope extends Node2D
 
 # Preloaded assets
-var tree_prefab := preload("res://scenes/tree.tscn");
+const tree_prefab := preload("res://scenes/tree.tscn");
 
 # References
 var play_button: Button;
@@ -15,45 +15,54 @@ var tree_snow: Sprite2D;
 var tree_emitter: GPUParticles2D;
 
 # Constants
-var TREE_TREE_TEXTURE = "TreeTexture";
-var TREE_SNOW_TEXTURE = "SnowTexture";
-var TREE_PARTICLES = "Particles";
-var TREE_SLALOM_AREA = "SlalomArea";
-var TREE_TRUNK_AREA = "TrunkArea";
-var TREE_DATA_TREE = "tree";
-var TREE_DATA_TEXTURE = "texture";
-var TREE_DATA_SNOW = "snow";
-var TREE_DATA_EMITTER = "emitter";
-var TREE_DATA_TWEEN = "tween";
+const TREE_TREE_TEXTURE := "TreeTexture";
+const TREE_SNOW_TEXTURE := "SnowTexture";
+const TREE_PARTICLES := "Particles";
+const TREE_SLALOM_AREA := "SlalomArea";
+const TREE_TRUNK_AREA := "TrunkArea";
+const TREE_DATA_TREE := "tree";
+const TREE_DATA_TEXTURE := "texture";
+const TREE_DATA_SNOW := "snow";
+const TREE_DATA_EMITTER := "emitter";
+const TREE_DATA_TWEEN := "tween";
 
 # Vectors
+const player_start_position := Vector2(500, -50);
+const player_play_position := Vector2(358, 335);
 var game_size: Vector2i;
-var player_start_position := Vector2(500, -50); #220
-var player_play_position := Vector2(358, 335);
 
 # Booleans
-var isPlaying := false;
-var isDown := false;
-var isCrashed := false;
+var is_game_started := false;
+var is_input_down := false;
+var is_player_crashed := false;
+var has_pressed_once := false;
+var animate_trees_round_start := false;
+var has_calculated_tree_size := false;
 
 # Numbers
-var player_middle_x := 210; #510;
-var player_tween_duration := 2.0;
-var player_rotation := 20;
-var player_speed := 350.0;
+const player_middle_x := 210;
+const player_tween_duration := 2.0;
+const player_rotation := 20;
+const player_speed := 350.0;
+const player_bounds_rotation := PI * 6.5;
+const player_bounds_distance := 250;
+const player_bounds_duration := 1.5;
+const player_bounds_end_delay := 2.0;
+const tree_speed := 500.0;
+const tree_animation_speed := 300.0;
+const tree_shake_angle := 6;
+const tree_shake_duration_short = 0.1;
+const tree_shake_duration_long = 0.5;
+const tree_fell_rotation := 1.3;
+const tree_fell_duration := 1.0;
+const tree_fell_delay := 0.5;
+const initial_tree_count := 10;
+const additional_tree_count := 1;
 var player_width := 0.0;
 var player_height := 0.0;
-var tree_speed := 500.0;
-var tree_width := 108;
-var tree_shake_angle := 6;
-var tree_shake_duration_short = 0.1;
-var tree_shake_duration_long = 0.5;
-var tree_fell_rotation := 1.3;
-var tree_fell_duration := 1.0;
-var tree_fell_delay := 0.5;
+var tree_width := 0.0;
+var tree_height := 0.0;
 var tree_reset_count := 0;
-var initial_tree_count := 10;
-var additional_tree_count := 1;
 
 # Other
 var current_trees: Array[Dictionary] = [];
@@ -71,9 +80,9 @@ func _ready() -> void:
 	game_size = get_viewport().get_visible_rect().size;
 
 	var player_size = player_sprite.texture.get_size();
-	var factor = scale.x;
-	player_width = player_size.x * factor;
-	player_height = player_size.y * factor;
+	var player_scale = player_sprite.scale.x;
+	player_width = player_size.x * player_scale;
+	player_height = player_size.y * player_scale;
 
 	play_button.pressed.connect(on_play_button_pressed);
 	player_area.area_entered.connect(on_player_area_entered);
@@ -81,17 +90,25 @@ func _ready() -> void:
 	setup_world();
 
 func _input(event: InputEvent) -> void:
-	if event is not InputEventScreenTouch or not isPlaying: return;
+	if event is not InputEventScreenTouch or not is_game_started: return;
 
-	var prev = isDown;
-	isDown = event.pressed;
-	if isDown != prev:
-		set_player_direction(isDown);
+	if not has_pressed_once:
+		has_pressed_once = not event.pressed;
+		return;
+
+	var prev = is_input_down;
+	is_input_down = event.pressed;
+	if is_input_down != prev:
+		set_player_direction(is_input_down);
 
 func _process(delta: float) -> void:
-	if not isPlaying: return;
+	var should_process = is_game_started and not is_player_crashed and has_pressed_once;
+	var should_process_trees = animate_trees_round_start or should_process;
 
-	update_trees(delta);
+	if should_process_trees:
+		update_trees(delta);
+
+	if not should_process: return;
 	update_player(delta);
 
 # Game flow callables
@@ -102,17 +119,23 @@ func setup_world() -> void:
 	reset_player();
 	animate_player_ready();
 
+	tree_reset_count = 0;
+	animate_trees_round_start = true;
+	has_pressed_once = false;
+
 	# Wait for the animation to finish, then enable the PlayButton.
 	await get_tree().create_timer(player_tween_duration).timeout;
+	animate_trees_round_start = false;
 	set_playbutton_state(PlayButtonState.Active);
 
 func round_start() -> void:
-	if isPlaying: return;
-	isPlaying = true;
+	if is_game_started: return;
+	is_game_started = true;
 
 func round_end() -> void:
-	if not isPlaying: return;
-	isPlaying = false;
+	if not is_game_started: return;
+	is_game_started = false;
+	player_sprite.visible = false;
 	clear_trees();
 
 	# here we can fade to black, before re-positioning things, and fading back in
@@ -131,10 +154,7 @@ func create_trees(count: int):
 		var emitter = tree.get_node(TREE_PARTICLES) as GPUParticles2D;
 
 		# Set values
-		var x = randi_range(0, game_size.x);
-		var y = randi_range(0, game_size.y);
-		var position := Vector2(x, y + game_size.y + 200);
-		tree.position = position;
+		tree.position = get_random_tree_position();
 		texture.flip_h = randi_range(0, 1) == 1;
 
 		# Finalize by adding to scene and saving in collection
@@ -148,14 +168,19 @@ func create_trees(count: int):
 		};
 		current_trees.push_back(data);
 
+		if not has_calculated_tree_size:
+			var tree_size = player_sprite.texture.get_size();
+			var tree_scale = player_sprite.scale.x;
+			tree_width = tree_size.x * tree_scale;
+			tree_height = tree_size.y * tree_scale;
+
 func reset_tree(tree_data: Dictionary) -> void:
 	var tree = tree_data["tree"];
 
 	var tween = tree_data["tween"];
 	if tween and tween.is_running(): tween.kill();
 
-	var position := Vector2i(randi_range(0, game_size.x), randi_range(game_size.y + 280, game_size.y + 380));
-	tree.position = position;
+	tree.position = get_random_tree_position(true);
 	tree.rotation = 0;
 
 	var snow = tree_data["snow"];
@@ -173,12 +198,15 @@ func reset_tree(tree_data: Dictionary) -> void:
 func update_trees(delta: float) -> void:
 	for tree_data in current_trees:
 		var tree = tree_data["tree"];
-		tree.position.y -= tree_speed * delta;
+		var speed = tree_speed if not animate_trees_round_start else tree_animation_speed;
+		tree.position.y -= speed * delta;
 
 		if tree.position.y < -50:
 			reset_tree(tree_data);
 
 func shake_tree(tree_data: Dictionary, isCrash: bool) -> void:
+	is_player_crashed = isCrash;
+
 	# Drop the snow cover
 	var snow = tree_data[TREE_DATA_SNOW];
 	var emitter = tree_data[TREE_DATA_EMITTER];
@@ -206,28 +234,34 @@ func shake_tree(tree_data: Dictionary, isCrash: bool) -> void:
 	if isCrash:
 		var fell_rotation = tree_fell_rotation if player_left_side else -tree_fell_rotation;
 		tween.set_ease(Tween.EASE_IN);
-		tween.tween_property(tree, "rotation", fell_rotation, tree_fell_duration).set_delay(tree_fell_delay);
-		await tween.finished;
-		round_end();
+		tween.tween_property(tree, "rotation", fell_rotation, tree_fell_duration);
+		tween.tween_callback(round_end).set_delay(tree_fell_delay);
 
 func clear_trees() -> void:
 	for tree_data in current_trees:
-		var tree = tree_data["tree"];
-		tree.queue_free;
+		var tree = tree_data["tree"] as Node2D;
+		tree.queue_free();
 
-	current_trees = [];
+	current_trees.clear();
+
+func get_random_tree_position(is_resetting := false) -> Vector2:
+	var min_y = game_size.y + tree_height;
+	var add_y = 300 if is_resetting else game_size.y;
+
+	var y = randi_range(min_y, min_y + add_y);
+	var x = randi_range(0, game_size.x);
+
+	return Vector2(x, y);
 
 # Player callables
 func update_player(delta: float) -> void:
-	if isCrashed: return;
-
-	move_player(delta, 1 if isDown else -1);
+	move_player(delta, 1 if is_input_down else -1);
 	check_player_bounds();
 
-func set_player_direction(isDown: bool) -> void:
-	var rotation = player_rotation if isDown else -player_rotation;
+func set_player_direction(is_input_down: bool) -> void:
+	var rotation = player_rotation if is_input_down else -player_rotation;
 	player_sprite.rotation_degrees = rotation;
-	player_sprite.flip_h = isDown;
+	player_sprite.flip_h = is_input_down;
 
 func move_player(delta: float, direction_multiplier: int) -> void:
 	var speed = player_speed * direction_multiplier;
@@ -240,13 +274,14 @@ func check_player_bounds() -> void:
 	var isOutsideRight = x > game_size.x + player_width;
 
 	if isOutsideLeft or isOutsideRight:
-		# TODO
-		round_end();
+		is_player_crashed = true;
+		animate_player_bounds();
 
 func reset_player() -> void:
 	player.position = player_start_position;
 	player_sprite.rotation_degrees = -player_rotation;
-	isCrashed = false;
+	player_sprite.visible = true;
+	is_player_crashed = false;
 
 func animate_player_ready() -> void:
 	var tween = create_tween();
@@ -254,7 +289,20 @@ func animate_player_ready() -> void:
 	tween.tween_property(player, "position", player_play_position, player_tween_duration);
 
 func animate_player_bounds() -> void:
-	pass;
+	var is_left_side = player.position.x < game_size.x / 2.0;
+	var half_width = 0;#player_width / 2.0;
+	var snap_position = half_width if is_left_side else game_size.x - half_width;
+	player.position.x = snap_position;
+
+	var bounds_distance = player_bounds_distance if is_left_side else -player_bounds_distance;
+	var bounds_rotation = player_bounds_rotation if is_left_side else -(player_bounds_rotation - PI);
+
+	var tween = player_sprite.create_tween();
+	tween.set_trans(Tween.TRANS_CUBIC);
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(player, "position:x", player.position.x + bounds_distance, player_bounds_duration);
+	tween.parallel().tween_property(player_sprite, "rotation", player_sprite.rotation + bounds_rotation, player_bounds_duration);
+	tween.tween_callback(round_end).set_delay(player_bounds_end_delay);
 
 func animate_player_crash() -> void:
 	pass;
@@ -284,6 +332,7 @@ func on_play_button_pressed() -> void:
 	round_start();
 
 func on_player_area_entered(area: Area2D) -> void:
+	if is_player_crashed: return;
 	if area.name != TREE_SLALOM_AREA and area.name != TREE_TRUNK_AREA: return;
 
 	var tree = area.get_parent();
@@ -297,3 +346,6 @@ func on_player_area_entered(area: Area2D) -> void:
 
 	var isCrash = area.name == TREE_TRUNK_AREA;
 	shake_tree(data, isCrash);
+
+	if isCrash:
+		animate_player_crash();
